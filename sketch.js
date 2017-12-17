fa2cdbca8757aaaa120d48c78d132d8dcf9c42c2
Indigo Lily <yulex.42@gmail.com>
2017-12-17 11:41:43 -0400

    Now scales with windows size

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ var width, height;
 function onresize() {
   width = cnv.width = window.innerWidth;
   height = cnv.height = window.innerHeight;
-  c.lineWidth = (width+height)/200;
+  c.lineWidth = (width+height)/175;
   c.strokeStyle = '#28f';
 }
 onresize();
@@ -17,25 +17,24 @@ const ripples = [];
 (function draw() {
   c.clearRect(0, 0, width, height);
 
-  if (Math.random() < 0.015) {
-    ripples.push({x: Math.random(), y: Math.random(), r: 0, fade:30 + Math.random()*30});
+  if (Math.random() < 0.03) {
+    ripples.push({x: Math.random(), y: Math.random(), size: 0, fade: 100, speed: 1 + Math.random() * 2});
   }
 
   for (let i = ripples.length - 1; i >= 0; i--) {
     let r = ripples[i];
-    if (r.r > width + height || r.fade <= 0) {
+    if (r.size > 1 || r.fade <= 0) {
       ripples.splice(i,1);
       continue;
     }
 
-    c.globalAlpha = r.fade/60;
+    c.globalAlpha = r.fade/100;
     c.beginPath();
-    c.arc(r.x*width, r.y*height, r.r, 0, Math.PI*2);
+    c.arc(r.x*width, r.y*height, r.size * (width+height), 0, Math.PI*2);
     c.stroke();
 
-    r.r += 10;
-    r.r *= 1.01;
-    r.fade--;
+    r.size += 0.003;
+    r.fade -= r.speed;
   }
 
   requestAnimationFrame(draw);
